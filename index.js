9025f2136a00bfb18a246d7e3828f4acad58882d
ali <ali_hasan97@hotmail.com>
2022-09-24 22:04:25 -0500

    added info route where number of entries is shown. Added route to view individual persons

--- index.js
+++ index.js
@@ -34,6 +34,24 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(
+        `Phonebook has info for ${persons.length} people <br/>
+        ${date}`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}`)
